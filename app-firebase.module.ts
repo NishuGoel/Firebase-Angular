39326637ff9e302ef8e2baaa533929259c582c64
NishuGoel <nishu0505@gmail.com>
2018-12-14 03:29:53 +0530

    Create app-firebase.module.ts

---
+++ app-firebase.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import {AngularFireModule} from 'angularfire2';
+import { environment } from 'src/environments/environment';
+
+import {AngularFireAuthModule} from 'angularfire2/auth';
+//import { Observable } from 'rxjs';
+
+
+@NgModule({
+
+  imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule
+     
+  ],
+  exports:[
+    AngularFireModule,
+    AngularFireAuthModule
+  ]
+})
+export class AppFirebaseModule { }
